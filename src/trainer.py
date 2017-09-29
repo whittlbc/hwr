f4ea3a22e9e6d5d2d600d9c40332d193afef4ffe
Ben Whittle <benwhittle31@gmail.com>
2017-09-29 16:39:17 -0700

    fixing last layer's activation to just be linear

--- src/trainer.py
+++ src/trainer.py
@@ -8,7 +8,7 @@ from src.dl.layers import fully_connected
 
 
 class Trainer:
-  batch_size = 30
+  batch_size = 32
   print_every = 10
   save_every = 100
   train_steps = 10000
@@ -19,14 +19,11 @@ class Trainer:
     # Since our images are of size 28x28 and we're using a fully-connected network (which can only take vectors
     # as inputs), our input size of the first layer should be 784 (28x28 with pixels unrolled).
 
-    # Since this is a multi-class classification problem with 10 classes, our final layer
-    # should have an output size of 10 and use the softmax activation function. This also means we should
-    # use a softmax_cross_entropy loss function.
-
     # Network Architecture (3 Layers):
     #   Layer 1: (784|300), FCL, ReLU activation
     #   Layer 2: (300|100), FCL, ReLU activation
-    #   Layer 3: (100|10), FCL, Softmax activation
+    #   Layer 3: (100|10), FCL, Linear activation
+    #   loss function applies softmax
     """
     print 'Extracting data...'
     # Get split train/test data
@@ -40,7 +37,7 @@ class Trainer:
     # Construct network
     o1 = fully_connected(self.x, output_units=300, activation=tf.nn.relu, scope='fc1')
     o2 = fully_connected(o1, output_units=100, activation=tf.nn.relu, scope='fc2')
-    y_hat = fully_connected(o2, output_units=10, activation=tf.nn.softmax, scope='fc3')
+    y_hat = fully_connected(o2, output_units=10, scope='fc3')
 
     # Define loss and loss optimizer
     self.loss = tf.reduce_mean(
